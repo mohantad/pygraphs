1cfd6dd82d17b376fd4b46eeb756200eda442719
MohantaD <deepak.mohanta@outlook.com>
2023-01-15 05:00:09 +0800

    plot bar and pie

--- pyplot-bar.py
+++ pyplot-bar.py
@@ -3,5 +3,5 @@ import matplotlib.pyplot as plt
 x=[1,2,3,4]
 y={'y=f(x)':[2,8,4,6], 'z=g(x)':[4,6,8,2]}
 df=pd.DataFrame(y, x)
-df.plot(kind='line', grid=True, title='My first line graph', xlabel='x  -->', ylabel='y=f(x), z=g(x)  -->', ylim=(1,10), xlim=(1,5))
+df.plot(kind='bar', grid=True, title='My first line graph', xlabel='x  -->', ylabel='y=f(x), z=g(x)  -->', ylim=(1,10), xlim=(1,5))
 plt.show()
