a29f193abc3db3210959c4241fd7ae3b0d661957
MohantaD <deepak.mohanta@outlook.com>
2023-01-16 02:44:17 +0800

    updated graphs and charts

--- pyplot.py
+++ pyplot.py
@@ -1,7 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-x=[1,2,3,4]
-y={'y=f(x)':[2,8,4,6], 'z=g(x)':[4,6,8,2]}
-df=pd.DataFrame(y, x)
-df.plot(kind='line', grid=True, title='My first line graph', xlabel='x  -->', ylabel='y=f(x), z=g(x)  -->', ylim=(1,10), xlim=(1,5))
+months=["Jan", "Feb", "Mar", "Apr"]
+temp=[2,8,4,6]
+aqi=[4,6,8,2]
+df=pd.DataFrame({'temp':temp, 'aqi':aqi}, months)
+df2=pd.DataFrame([[temp[0],aqi[0]],[temp[1],aqi[1]],[temp[2],aqi[2]], [temp[3],aqi[3]]], columns=["temp", "aqi"])
+
+df.plot(kind='line', title='Temperature~Air quality line chart', grid=True, xlabel='months  -->', ylabel='temp, aqi  -->', ylim=(1,10), xlim=(0,5), figsize=(10, 8))
+df.plot(kind='bar', title='Temperature~Air quality bar chart', grid=True, xlabel='months  -->', ylabel='temp, aqi  -->', ylim=(1,10), xlim=(0,5), figsize=(10, 8))
+df.plot(kind='area', title='Temperature~Air quality area chart', grid=True, xlabel='months  -->', ylabel='temp, aqi  -->', ylim=(1,10), xlim=(0,5), figsize=(10, 8))
+df2.plot(kind='scatter', title='Temperature~Air quality scatter chart', x="temp", y="aqi", s=100, c=['tomato', 'aqua', 'peachpuff', 'yellowgreen'], grid=True, xlabel='aqi  -->', ylabel='temp -->', ylim=(0,10), xlim=(0,10), figsize=(10, 8))
+df.plot(kind='pie', title='Temperature~Air quality Pie chart', subplots=True, shadow = True, startangle = 180, explode =(0,0.05,0,0.05),  autopct ='%1.1f%%', colors=['tomato', 'aqua', 'peachpuff', 'yellowgreen'], figsize=(10, 8))
 plt.show()
